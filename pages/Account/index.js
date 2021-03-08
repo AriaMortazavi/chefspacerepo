7b03cc63b96ca7511dfde5e7ae7ce3f88e817d0c
shannonmaynard <55810743+shannonmaynard@users.noreply.github.com>
2021-03-08 11:35:37 -0800

    .

---
+++ pages/Account/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+
+import axios from 'axios';
+
+let fakedb = [
+
+]
+
+const Account = () => {
+
+  return (
+    <div className="Account">
+      <Template></Template>
+    </div>
+  );
+};
+
+export default Account;
