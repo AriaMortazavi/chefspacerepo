1efcd38089b69ffbe5dd002fcb817dc728eb1de3
AriaMortazavi <55810685+AriaMortazavi@users.noreply.github.com>
2021-03-24 16:32:07 -0700

    updated front end connection for accounts

--- pages/Account/index.js
+++ pages/Account/index.js
@@ -9,21 +9,38 @@ import Input from "../../comps/Input";
 import NavHome from "../../comps/NavHome";
 import axios from "axios";
 
-let fakedb = [];
 
 const Account = () => {
+
+
+const [newUserInfo, setNewUserInfo] = useState(null);
+const [newEmailInfo, setNewEmailInfo] = useState(null);
+const [newLevelInfo, setNewLevelInfo] = useState(null);
+
+
+const ChangeInfoHandler = async () => {
+  console.log(newUserInfo);
+  console.log(newEmailInfo);
+  console.log(newLevelInfo);
+const res = await axios.post("https://chefspace-backend.herokuapp.com/users", {
+username: newUserInfo, email: newEmailInfo, level: newLevelInfo
+  }
+);
+alert("Info has been updated")
+}
+
   return (
     <div className="Account">
       <NavAccount />
       <div className="contents">
       <br></br>
         <AccountImg />
-        <Input text="Username" placeholder="" />
-        <Input text="Email" placeholder="" />
+        <Input onChange={(e) => setNewUserInfo(e.target.value)} text="Username" placeholder="" />
+        <Input onChange={(e) => setNewEmailInfo(e.target.value)} text="Email" placeholder="" />
         <br></br>
-        <Hats text="Change Skill Level" />
+        <Hats onChange={(e) => setNewLevelInfo(e.target.value)} text="Change Skill Level" />
         <br></br><br></br><br></br>
-        <FormButtons text="Save Changes" /> 
+        <FormButtons onClick={ChangeInfoHandler} text="Save Changes" /> 
       </div>
       <div className="logout">
         <Logout />
