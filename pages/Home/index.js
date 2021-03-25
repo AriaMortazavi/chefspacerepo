11db8e3fe3026b2fa20a3e2a7b5c319418e41250
Farhaz Ladha <farhaz@iMac.hitronhub.home>
2021-03-25 15:09:16 -0700

    posting/image upload functions

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -7,19 +7,64 @@ import AccountImg from "../../comps/AccountImg";
 import FormButtons from "../../comps/FormButtons";
 import Input from "../../comps/Input";
 import NavHome from "../../comps/NavHome";
-import axios from "axios";
 import Post from '../../comps/Post';
 import MessageBox from '../../comps/MessageBox';
+import axios from "axios";
 
 let fakedb = [];
 
+
 const Home = () => {
+
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState();
+
+  const submitPost = async event => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("description", description);
+
+    const result = await axios.post('https://chefspace-backend.herokuapp.com/images', formData, { headers: {'Content-Type':'multipart/form-data'}})
+    setImage(result.data.imagePath)
+  }
+  
+  // const [postDescription, setDescription] = useState(null);
+  // const [postImage, setImage] = useState(null);
+  // const [postLikes, setLikes] = useState(null);
+  // const [postDislikes, setDislikes] = useState(null);
+
+  // const sendPost = async () => {
+  //   console.log(description);
+  //   console.log(image);
+  //   console.log(likes);
+  //   console.log(dislikes);
+
+  //   const res = await axios.post("https://chefspace-backend.herokuapp.com/posts", {description: postDescription, image: postImage, likes: postLikes, dislikes: postDislikes});
+
+  //   console.log(res);
+  // }
+  
   return (
     <div className="Account">
       <NavHome />
       <div className="contents">
 
-        <Post
+      <Post
+          FullName="Gordon Ramsay"
+          UserName="g.ramsay"
+          profileSrc="https://b.thumbs.redditmedia.com/NAzeLE5Ca6fjDQhBFgL5Rtffo7datdqzUkY__3XnD2c.png"
+          ratingSrc="hat3.svg"
+          PostText={setDescription}
+          // LinkUrl="http://ow.ly/54ul50Dy9Br"
+          postImgSrc={setImage}
+          TastyCount="0"
+          NotTastyCount="0"
+        />
+
+        {/* <Post
           FullName="Gordon Ramsay"
           UserName="g.ramsay"
           profileSrc="https://b.thumbs.redditmedia.com/NAzeLE5Ca6fjDQhBFgL5Rtffo7datdqzUkY__3XnD2c.png"
@@ -29,9 +74,9 @@ const Home = () => {
           postImgSrc="https://saporitokitchen.com/wp-content/uploads/2021/02/tomato-soup-with-bread.jpg"
           TastyCount="123"
           NotTastyCount="45"
-        />
+        /> */}
 
-        <Post
+        {/* <Post
           FullName="Bobby Flay"
           UserName="b_flay"
           profileSrc="https://64.media.tumblr.com/avatar_bf60041fee99_128.pnj"
@@ -41,11 +86,11 @@ const Home = () => {
           postImgSrc="https://helenscchin.files.wordpress.com/2020/09/bacon-egg-and-chocolate-croque-monsiuer-with-blueberries-.jpg"
           TastyCount="123"
           NotTastyCount="45"
-        />
+        /> */}
         
       </div>
 
-      <MessageBox/>
+      <MessageBox submit={submitPost} onChangeUpload={e => setFile(e.target.files[0])} onChangeDescription={e => setDescription(e.target.value)}/>
       
     </div>
   );
